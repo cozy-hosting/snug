0c6b04648c6d01f29f0b61e965c35d7971bcf3c2
Samuel Oechsler <samuel@oechsler.it>
2021-02-26 11:34:44 +0100

    Improve port syntax

--- scripts/example.snug.kts
+++ scripts/example.snug.kts
@@ -1,18 +1,27 @@
 import it.oechsler.script.language.DeploymentBuilder.Companion.deploy
-import it.oechsler.script.data.Image
-import it.oechsler.script.data.Port
+import it.oechsler.script.language.PortBuilder
 
-deploy {
-    name = "nginx"
+fun PortBuilder.minecraft(pod: Int = 25565) =
+    port(25565, pod, "minecraft")
+
+deploy("nginx") {
     tags("personal", "website")
     replicas = 3
 
-    container {
-        name="proxy"
-        image = Image("nginx", "latest")
+    container("proxy") {
+        image("nginx", "latest")
+
         ports {
-            5000 to 8080
-            80 to 8081
+            minecraft()
+
+            port(21, 5642, "ftp")
+            port(443, 4043)
+            port(45, "something")
+            port(22)
+
+            // Shorthand syntax
+            "http" with 80 to 8080
+            9000 to 8081
         }
     }
 }
